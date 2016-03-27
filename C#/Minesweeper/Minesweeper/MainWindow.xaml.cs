52af2fe4d8f5970d6b3bf973cb88837bc2e6b82d
Lars Wefald <larwef@gmail.com>
2016-03-27 19:54:51 +0200

    Added C# WPF Mineweeper
    Minesweeper game made wit C# and WPF in Visual Studio. Currently
    working. It displays how many mines are in the adjacent tiles, hae win
    and loose condition and a new game can be started by clicking start game
    again.
    
    It does not handle big board well. The GUI could be improved quite bit.
    Another suggested improvement is to make the adjacent tiles with zeros
    open automatically.

---
+++ C#/Minesweeper/Minesweeper/MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Minesweeper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int[,] board;
+        int openedBlocks;
+        int width;
+        int height;
+        int mines;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void startBtn_Click(object sender, RoutedEventArgs e)
+        {
+            gridContainer.Children.Clear();
+
+            openedBlocks = 0;
+
+            width = int.Parse(inputWidth.Text);
+            height = int.Parse(inputHeight.Text);
+            mines = int.Parse(inputMines.Text);
+
+            init_board(width, height, mines);
+
+            border.Width = width * 25;
+            border.Height = height * 25;
+
+            gridContainer.Columns = width;
+            gridContainer.Rows = height;
+            for (int i = 0; i < gridContainer.Columns * gridContainer.Rows; i++)
+            {
+                Rectangle block = new Rectangle();
+                block.Fill = new SolidColorBrush(Colors.Black);
+                block.Margin = new Thickness(0.5);
+                block.Cursor = Cursors.Hand;
+                block.MouseLeftButtonDown += block_click;
+                gridContainer.Children.Add(block);
+            }
+            gridContainer.IsEnabled = true;
+        }
+
+        void block_click(object sender, RoutedEventArgs e)
+        {
+            var clickedBlock = (sender as Rectangle);
+
+            int rows = gridContainer.Rows;
+            int columns = gridContainer.Columns;
+
+            int index = gridContainer.Children.IndexOf(clickedBlock);
+
+            int column = index % columns;
+            int row = index / columns;
+
+            string imageUrl = "pack://application:,,,/Images/" + board[column,row].ToString() + ".png";
+
+            clickedBlock.Fill = new ImageBrush
+            {
+                ImageSource = new BitmapImage(new Uri(imageUrl))
+            };
+            clickedBlock.IsEnabled = false;
+
+            openedBlocks++;
+
+            if (board[column,row] == 9)
+            {
+                MessageBox.Show("You lost");
+                gridContainer.IsEnabled = false;
+            }
+
+            else if (openedBlocks >= height*width - mines)
+            {
+                MessageBox.Show("You won!");
+                gridContainer.IsEnabled = false;
+            }
+        }
+
+        void init_board(int rows, int columns, int mines)
+        {
+            board = new int[rows, columns];
+            int minesPlaced = 0;
+
+            Random rnd = new Random();
+
+            while (minesPlaced < mines)
+            {
+                int x = rnd.Next(columns);
+                int y = rnd.Next(rows);
+
+                if (board[x,y] != 9)
+                {
+                    board[x,y] = 9;
+                    updateAdjacentBlocks(x,y);
+                    minesPlaced++;
+                }
+            }
+        }
+
+        void updateAdjacentBlocks(int x, int y)
+        {
+            int xLow = x - 1;
+            int xHigh = x + 1;
+            int yLow = y - 1;
+            int yHigh = y + 1;
+
+            if (xLow < 0)
+            {
+                xLow = 0;
+            }
+            if (xHigh >= board.GetLength(0))
+            {
+                xHigh = board.GetLength(0) - 1;
+            }
+            if (yLow < 0)
+            {
+                yLow = 0;
+            }
+            if (yHigh >= board.GetLength(1))
+            {
+                yHigh = board.GetLength(1) - 1;
+            }
+
+            for (int i = xLow; i <= xHigh; i++)
+            {
+                for (int j = yLow; j <= yHigh; j++)
+                {
+                    if(board[i,j] != 9)
+                    {
+                        board[i,j]++;
+                    }
+                }
+            }
+        }
+    }
+}
